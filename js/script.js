048b8286fb94e20b8b4ea817bbd89bbb69568b99
asadullah055 <asadullahahmed01@gmail.com>
2022-08-11 14:53:37 +0600

    second commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const swiper1 = new Swiper(".swiper1", {
+  loop: true,
+  spaceBetween: 50,
+  grabCursor: true,
+});
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  spaceBetween: 30,
+  grabCursor: true,
+  slidesPerView: 4,
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    577: {
+      slidesPerView: 1,
+    },
+    767: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
